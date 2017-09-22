c8a24bc382c335bddc02f97f71bbca35689cf70c
Sakthikprakash <sakthikprakashc@hotmail.com>
2017-09-22 23:15:57 +0530

    Initial commit

---
+++ FileManagementSystem.cs
@@ -0,0 +1,234 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Arie.DAO
+{
+    class FileManagementSystem
+    {
+        string message = string.Empty;
+
+        /*      CHECK FOR THE FOLDER ALREADY EXISTS OR NOT      */
+        public void checkForFolderExistsOrNot(string folderPath, string fileName, string textContent)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(folderPath)
+                    && !string.IsNullOrEmpty(fileName))
+                {
+                    if (Directory.Exists(folderPath))
+                    {
+                        checkForFileExistsOrNot(folderPath + @"\" + fileName + ".txt", textContent);
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory(folderPath);
+                        checkForFileExistsOrNot(folderPath + @"\" + fileName + ".txt", textContent);
+                    }
+                }
+            }
+            catch (IOException ioe)
+            {
+                message = "Inside checkForFolderExistsOrNot()..\n" + ioe.ToString();
+            }
+        }
+
+        /*      CHECK FOR THE FILE ALREADY EXISTS OR NOT      */
+        public void checkForFileExistsOrNot(string filePath, string myMessageContent)
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    appendFileContent(filePath, myMessageContent);
+                }
+                else
+                {
+                    writeFileContent(myMessageContent + "\n", filePath, 1);
+                }
+            }
+            catch (IOException ioe)
+            {
+                message = "Inside checkForFileExistsOrNot()..\n" + ioe.ToString();
+            }
+        }
+
+        /*      APPENDING THE FILE CONTENT      */
+        public void appendFileContent(string filePath, string fileContent)
+        {
+            try
+            {
+                StreamWriter streamWriter = File.AppendText(filePath);
+                //streamWriter.WriteLine(fileContent);
+                streamWriter.Write(fileContent);
+                streamWriter.Close();
+            }
+            catch (IOException ioe)
+            {
+                message = "appendFileContent()..\n" + ioe.ToString();
+            }
+        }
+
+        /*      CREATING THE FOLDER NAME      */
+        public bool createFolder(string folderName)
+        {
+            try
+            {
+                return Directory.CreateDirectory(folderName).Exists;
+            }
+            catch (IOException ioe)
+            {
+                message = "Inside createFolder()..\n" + ioe.ToString();
+                return false;
+            }
+        }
+
+        /*      READ THE CONTENT FOR THE GIVEN FILE NAME    */
+        public string readFileContent(string fileNameWithTypeAndLocation, int fileReadMode)
+        {
+            try
+            {
+                string str = string.Empty;
+                switch (fileReadMode)
+                {
+                    case 1:     //////          READING DATA USING StreamReader     //
+                        StreamReader streamReadObject = new StreamReader(fileNameWithTypeAndLocation);
+                        str = streamReadObject.ReadToEnd().ToString();
+                        streamReadObject.Close();
+                        break;
+                    case 2:     //////          READING DATA USING File             //
+                        str = File.ReadAllText(fileNameWithTypeAndLocation);
+                        break;
+                    case 3:     //////          READING DATA USING FileStream       //
+                        FileStream fileStreamObject = new FileStream(fileNameWithTypeAndLocation, FileMode.Open);
+                        byte[] byteArray = new byte[(int)fileStreamObject.Length];
+                        int nBytesRead = fileStreamObject.Read(byteArray, 0, (int)fileStreamObject.Length);
+                        str = Encoding.ASCII.GetString((byteArray));
+                        fileStreamObject.Close();
+                        break;
+                    case 4:     //////          READING DATA USING TextReader       //
+                        TextReader textReader = File.OpenText(fileNameWithTypeAndLocation);
+                        str = textReader.ReadToEnd();
+                        textReader.Close();
+                        break;
+                    case 5:     //////          READING DATA USING BinaryReader     //
+                        BinaryReader binaryReader = new BinaryReader(File.Open(fileNameWithTypeAndLocation, FileMode.Open));
+                        str = binaryReader.ReadString();
+                        binaryReader.Close();
+                        break;
+                    default:
+                        break;
+                }
+                return str;
+            }
+            catch (IOException ioe)
+            {
+                message = "Inside readFileContent()..\n" + ioe.ToString();
+                return null;
+            }
+        }
+
+        /*      WRITE THE NEW CONTENT FOR THE GIVEN FILE NAME WITH PATH LOCATION    */
+        public void writeFileContent(string fileContent, string fileNameWithTypeAndLocation, int fileWriteMode)
+        {
+            try
+            {
+                string str = string.Empty;
+                switch (fileWriteMode)
+                {
+                    case 1:    //////          WRITING DATA USING StreamWriter      //
+                        StreamWriter streamWriteObject = new StreamWriter(fileNameWithTypeAndLocation);
+                        streamWriteObject.Write(fileContent + str);
+                        streamWriteObject.Close();
+                        break;
+                    case 2:     //////          WRITING DATA USING File             //
+                        File.WriteAllText(fileNameWithTypeAndLocation, fileContent + str);
+                        break;
+                    case 3:     //////          WRITING DATA USING FileStream       //
+                        byte[] byteData = null;
+                        byteData = Encoding.ASCII.GetBytes(fileContent + str);
+                        FileStream fileStreamObject = new FileStream(fileNameWithTypeAndLocation, FileMode.Append);
+                        fileStreamObject.Write(byteData, 0, byteData.Length);
+                        fileStreamObject.Close();
+                        break;
+                    case 4:     //////          READING DATA USING TextWriter       //
+                        TextWriter textWriter = File.CreateText(fileNameWithTypeAndLocation);
+                        textWriter.Write(fileContent + str);
+                        textWriter.Close();
+                        break;
+                    case 5:     //////          READING DATA USING BinaryWriter     //
+                        BinaryWriter binaryWriter = new BinaryWriter(File.Open(fileNameWithTypeAndLocation, FileMode.Create));
+                        binaryWriter.Write(fileContent + str);
+                        binaryWriter.Close();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (IOException ioe)
+            {
+                message = "Inside writeFileContent()..\n" + ioe.ToString();
+            }
+        }
+
+        public bool deleteFile(string deleteFilePath)
+        {
+            try
+            {
+                File.Delete(deleteFilePath);
+                message = "FILE DELETED SUCCESSFULLY..\n";
+                return true;
+            }
+            catch (IOException ioe)
+            {
+                message = "deleteFile()..\n" + ioe.ToString();
+                return false;
+            }
+        }
+
+        public bool copyFileTo(string sourceFilePath, string destinationFilePath)
+        {
+            try
+            {
+                if (File.Exists(sourceFilePath))
+                {
+                    File.Copy(sourceFilePath, destinationFilePath);
+                    message = "FILE COPIED FROM [" + sourceFilePath + "] TO [" + destinationFilePath + "]";
+                    return true;
+                }
+                else
+                {
+                    message = "FILE NOT EXISTS IN THE [" + sourceFilePath + "] PATH";
+                    return false;
+                }
+            }
+            catch (IOException ioe)
+            {
+                message = "Inside copyFileTo()..\n" + ioe.ToString();
+                return false;
+            }
+        }
+
+        public bool moveFileTo(string sourceFilePath, string destinationFilePath)
+        {
+            try
+            {
+                if (File.Exists(sourceFilePath))
+                {
+                    File.Move(sourceFilePath, destinationFilePath);
+                    message = "FILE MOVED FROM [" + sourceFilePath + "] TO [" + destinationFilePath + "]";
+                    return true;
+                }
+                else
+                {
+                    message = "FILE NOT EXISTS IN THE [" + sourceFilePath + "] PATH";
+                    return false;
+                }
+            }
+            catch (IOException ioe)
+            {
+                message = "Inside moveFileTo()..\n" + ioe.ToString();
+                return false;
+            }
+        }
+    }
+}
---
+++ MSSQLDBConnectivity.cs
@@ -0,0 +1,426 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Arie.DAO
+{
+    public class MSSQLDBConnectivity
+    {
+        SqlConnection connection = null;
+        SqlCommand command = null;
+        SqlDataAdapter sqlAdapter = null;
+        SqlParameter sqlParameter = null;
+
+        string connectionString = string.Empty;
+
+        public string message = string.Empty, sPath = string.Empty;
+
+        //FileManagementSystem fileManagementSystem = new FileManagementSystem();
+        string startDateTime = string.Empty, endDateTime = string.Empty;
+
+        public MSSQLDBConnectivity(string storeOfflineFileNameWithPath)
+        {
+            sPath = storeOfflineFileNameWithPath;
+        }
+
+        /*      OPENING THE DATABASE CONNECTIVITY IF ALREADY CLOSED     */
+        public string openMyDataBase(string dataContent)
+        {
+            try
+            {
+                if (connection != null && connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                return dataContent;
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside openMyDataBase()..\n" + sqlExp.ToString();
+                return message;
+            }
+        }
+
+        /*      CLOSING THE DATABASE CONNECTIVITY IF ALREADY OPENED      */
+        public string closeMyDataBase(string dataContent)
+        {
+            try
+            {
+                if (connection != null && connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+                return dataContent;
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside closeMyDataBase()..\n" + sqlExp.ToString();
+                return message;
+            }
+        }
+
+        /*      ESTABLISHING THE DATABASE CONNECTION BY PROVINDING THE CONNECTION STRING   */
+        public bool dataBaseConnection(string serverName, string dataBaseName, string userName, string password)
+        {
+            try
+            {
+                string stringConnection = @"Data Source=" + serverName +
+                                            ";Initial Catalog=" + dataBaseName +
+                                            ";User ID=" + userName +
+                                            ";Password=" + password +
+                                            ";connection timeout=900;";
+                connection = new SqlConnection(stringConnection);
+                connection.Open();
+                return true;
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside dataBaseConnection()..\n" + sqlExp.Message;
+                return false;
+            }
+        }
+
+        /*      ESTABLISHING THE DATABASE CONNECTION BY PROVINDING THE CONNECTION STRING   */
+        public bool dataBaseConnection(string connString)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(connString))
+                {
+                    connection = new SqlConnection(connString);
+                    connection.Open();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside dataBaseConnection()..\n" + sqlExp.Message;
+                return false;
+            }
+        }
+
+        /*      PROVIDING THE NORMAL STRING QUERY TO GET THE RESULT IN DATATABLE FORMAT       */
+        public bool dataBaseCommand(string queryStatement, bool isQuery, ref DataTable dataTable, ref int updateCount, string folderPath, string fileName)
+        {
+            string stringBuilding = string.Empty;
+            try
+            {
+                startDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffff");
+                stringBuilding += openMyDataBase("OPENED BY [" + startDateTime + "] ..\n");
+                command = new SqlCommand(queryStatement, connection);
+                if (isQuery)
+                {
+                    dataTable = new DataTable();
+                    sqlAdapter = new SqlDataAdapter(command);
+                    sqlAdapter.Fill(dataTable);
+                }
+                else
+                {
+                    updateCount = command.ExecuteNonQuery();
+                }
+                dataTable.AcceptChanges();
+                stringBuilding += "INSIDE TRY..\n";
+                return true;
+            }
+            catch (SqlException sqlExp)
+            {
+                message = sqlExp.ToString();
+                stringBuilding += "INSIDE EXCEPTION..[" + message + "]..\n";
+                return false;
+            }
+            finally
+            {
+                endDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffff");
+                stringBuilding += " - " + Convert.ToDateTime(endDateTime).Subtract(Convert.ToDateTime(startDateTime)) + "\n";
+                stringBuilding += closeMyDataBase("FINALLY CLOSED..\n");
+                //fileManagementSystem.checkForFolderExistsOrNot(folderPath, fileName, stringBuilding);
+            }
+        }
+
+        /*      PROVIDING THE PROCEDURE NAME WITH LOCK TO GET THE RESULT IN DATATABLE FORMAT       */
+        public bool callStoredProcedure(string procedureName, bool isQuery, Object[,] variables, ref DataSet dataSet, ref int updateCount
+            , string folderPath, string fileName, ref string errorMessage)
+        {
+            SqlTransaction transaction = null;
+            string stringBuilding = string.Empty;
+            //message = string.Empty;
+            int variable_Count = 0;
+            try
+            {
+                startDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffff");
+                stringBuilding += openMyDataBase("OPENED BY [" + startDateTime + "] ..\n");
+                transaction = connection.BeginTransaction(IsolationLevel.Serializable);
+                command = new SqlCommand(procedureName, connection, transaction);
+                command.CommandType = CommandType.StoredProcedure;
+                if (variables != null)
+                {
+                    variable_Count = variables.Length / 2;
+                    for (int i = 0; i < variable_Count; i++)
+                    {
+                        stringBuilding += "@" + variables[i, 0] + " = " + variables[i, 1] + "\n";
+                        sqlParameter = new SqlParameter("@" + variables[i, 0], variables[i, 1]);
+                        command.Parameters.Add(sqlParameter);
+                    }
+                }
+                if (isQuery)
+                {
+                    dataSet = new DataSet();
+                    sqlAdapter = new SqlDataAdapter(command);
+                    sqlAdapter.Fill(dataSet);
+                }
+                else
+                {
+                    updateCount = command.ExecuteNonQuery();
+                }
+                transaction.Commit();
+                dataSet.AcceptChanges();
+                stringBuilding += "INSIDE TRY..\n";
+                return true;
+            }
+            catch (Exception exception)
+            {
+                if (transaction != null)
+                {
+                    transaction.Rollback();
+                }
+                errorMessage = exception.ToString();
+                stringBuilding += "INSIDE EXCEPTION..[" + errorMessage + "]..\n";
+                return false;
+            }
+            finally
+            {
+                endDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffff");
+                stringBuilding += procedureName + " - " + Convert.ToDateTime(endDateTime).Subtract(Convert.ToDateTime(startDateTime)) + "\n";
+                stringBuilding += closeMyDataBase("FINALLY CLOSED..\n");
+                //fileManagementSystem.checkForFolderExistsOrNot(folderPath, fileName, stringBuilding);
+            }
+        }
+
+        /*      PROVIDING THE STRING QUERY WITH LOCK TO GET THE RESULT IN DATATABLE FORMAT       */
+        public bool dataBaseLockCommand(string queryStatement, bool isQuery, ref DataSet dataSet, ref int updateCount, string folderPath, string fileName)
+        {
+            SqlTransaction transaction = null;
+            string stringBuilding = string.Empty;
+            try
+            {
+                startDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffff");
+                stringBuilding += openMyDataBase("OPENED BY [" + startDateTime + "] ..\n");
+                transaction = connection.BeginTransaction(IsolationLevel.Serializable);
+                command = new SqlCommand(queryStatement, connection, transaction);
+                command.CommandTimeout = 0;
+                //command.CommandText = queryStatement;
+                //command.Connection = connection;                
+                //command.Transaction = transaction;
+                if (isQuery)
+                {
+                    dataSet = new DataSet();
+                    sqlAdapter = new SqlDataAdapter(command);
+
+                    sqlAdapter.Fill(dataSet);
+                }
+                else
+                {
+                    updateCount = command.ExecuteNonQuery();
+                }
+                transaction.Commit();
+                dataSet.AcceptChanges();
+                stringBuilding += "INSIDE TRY..\n";
+                return true;
+            }
+            catch (SqlException sqlExp)
+            {
+                if (transaction != null)
+                {
+                    transaction.Rollback();
+                }
+                message = sqlExp.ToString();
+                stringBuilding += "INSIDE EXCEPTION..[" + message + "]..\n";
+                return false;
+            }
+            finally
+            {
+                endDateTime = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fffff");
+                stringBuilding += " - " + Convert.ToDateTime(endDateTime).Subtract(Convert.ToDateTime(startDateTime)) + "\n";
+                stringBuilding += closeMyDataBase("FINALLY CLOSED..\n");
+                //fileManagementSystem.checkForFolderExistsOrNot(folderPath, fileName, stringBuilding);
+            }
+        }
+
+        /*      DISPLAY THE INFORMATION       */
+        public string showDBInformation()
+        {
+            return message;
+        }
+
+        /*      TO GET THE DATABASE LIST FROM THE SERVER       */
+        public ArrayList getListOfDataBases()
+        {
+            try
+            {
+                ArrayList dataBaseArrayList = new ArrayList();
+                DataTable dataTable = new DataTable();
+                int count = 0;
+                string getDataBaseListQuery = "SELECT name FROM master..sysdatabases WHERE name NOT IN ('master', 'tempdb', 'model', 'msdb');";
+
+                if (dataBaseCommand(getDataBaseListQuery, true, ref dataTable, ref count, null, null))
+                {
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        dataBaseArrayList.Add(dataTable.Rows[i]["name"].ToString());
+                    }
+                    getDataBaseListQuery = string.Empty;
+                    return dataBaseArrayList;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside getListOfDataBases()..\n" + sqlExp.ToString();
+                return null;
+            }
+        }
+
+        /*      TO GET THE TABLE NAME LIST FOR THE CHOOSEN DATABASE       */
+        public ArrayList getListOfTables(string dataBaseName)
+        {
+            try
+            {
+                ArrayList tableArrayList = new ArrayList();
+                DataTable dataTable = new DataTable();
+                int count = 0;
+                string getTableListQuery = "SELECT TABLE_NAME FROM " + dataBaseName + ".INFORMATION_SCHEMA.TABLES";
+
+                if (dataBaseCommand(getTableListQuery, true, ref dataTable, ref count, null, null))
+                {
+                    for (int i = 0; i < dataTable.Rows.Count; i++)
+                    {
+                        tableArrayList.Add(dataTable.Rows[i]["TABLE_NAME"].ToString());
+                    }
+                    getTableListQuery = string.Empty;
+                    return tableArrayList;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside getListOfTables()..\n" + sqlExp.ToString();
+                return null;
+            }
+        }
+
+        /*      TO GET THE FIELD NAME LIST FOR THE CHOOSEN TABLE NAME       */
+        public ArrayList getFieldNameListForTable(string dataBaseName, string tableName)
+        {
+            try
+            {
+                openMyDataBase("OPEN CONNECTION..\n");
+                {
+                    ArrayList fieldNameArrayList = new ArrayList();
+                    DataTable dataTable = new DataTable();
+                    int count = 0;
+                    string getFieldNameListQuery = "SELECT COLUMN_NAME FROM " + dataBaseName + ".INFORMATION_SCHEMA.COLUMNS" +
+                                                    " WHERE TABLE_NAME = '" + tableName + "'";
+
+                    if (dataBaseCommand(getFieldNameListQuery, true, ref dataTable, ref count, null, null))
+                    {
+                        for (int i = 0; i < dataTable.Rows.Count; i++)
+                        {
+                            fieldNameArrayList.Add(dataTable.Rows[i]["COLUMN_NAME"].ToString());
+                        }
+                        getFieldNameListQuery = string.Empty;
+                        return fieldNameArrayList;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside getFieldNameListForTable()..\n" + sqlExp.ToString();
+                return null;
+            }
+        }
+
+        /*      TO GET THE FIELD TYPE FOR THE GIVEN FIELD NAME      */
+        public string getFieldTypeForTables(string dataBaseName, string tableName, string fieldName)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+                int count = 0;
+                string getFieldTypeListQuery = "SELECT DATA_TYPE FROM " + dataBaseName + ".INFORMATION_SCHEMA.COLUMNS" +
+                                                " WHERE TABLE_NAME = '" + tableName + "'" +
+                                                " AND COLUMN_NAME = '" + fieldName + "'";
+
+                if (dataBaseCommand(getFieldTypeListQuery, true, ref dataTable, ref count, null, null))
+                {
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        return dataTable.Rows[0]["DATA_TYPE"].ToString();
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside getFieldTypeForTables()..\n" + sqlExp.ToString();
+                return null;
+            }
+        }
+
+        /*      TO GET THE DEFAULT VALUE OF FIELD TYPE FOR THE GIVEN FIELD NAME      */
+        public bool getDefaultValueOfFieldTypeForTables(string dataBaseName, string tableName, string fieldName)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+                int count = 0;
+                string getFieldTypeListQuery = "SELECT IS_NULLABLE FROM " + dataBaseName + ".INFORMATION_SCHEMA.COLUMNS" +
+                                                        " WHERE TABLE_NAME = '" + tableName + "'" +
+                                                        " AND COLUMN_NAME = '" + fieldName + "'";
+
+                if (dataBaseCommand(getFieldTypeListQuery, true, ref dataTable, ref count, null, null))
+                {
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        return dataTable.Rows[0]["IS_NULLABLE"].ToString().Equals("Yes", StringComparison.OrdinalIgnoreCase) ? true : false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (SqlException sqlExp)
+            {
+                message = "Inside getDefaultValueOfFieldTypeForTables()..\n" + sqlExp.ToString();
+                return false;
+            }
+        }
+    }
+}
